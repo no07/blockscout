ce99d64ea9b7b7ac5ddbdff63d3d10ab6f1cb966
no07 <nhatquang.vu@gmail.com>
2020-08-25 10:55:27 +0700

    first commit

---
+++ apps/indexer/config/prod/geth.exs
@@ -0,0 +1,20 @@
+use Mix.Config
+
+config :indexer,
+  block_interval: :timer.seconds(5),
+  json_rpc_named_arguments: [
+    transport: EthereumJSONRPC.HTTP,
+    transport_options: [
+      http: EthereumJSONRPC.HTTP.HTTPoison,
+      url: "https://chain.ligerstar.com",
+      http_options: [recv_timeout: :timer.minutes(1), timeout: :timer.minutes(1), hackney: [pool: :ethereum_jsonrpc]]
+    ],
+    variant: EthereumJSONRPC.Geth
+  ],
+  subscribe_named_arguments: [
+    transport: EthereumJSONRPC.WebSocket,
+    transport_options: [
+      web_socket: EthereumJSONRPC.WebSocket.WebSocketClient,
+      url: "wss://chain.ligerstar.com/ws"
+    ]
+  ]
